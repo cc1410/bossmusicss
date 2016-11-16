4d0c7fc2d721f6c5429895b62fb2590ab953e527
cc1410 <chen986755611@gmail.com>
2016-11-16 17:26:20 +0100

    homepage

---
+++ home2/homepage.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link href="csshomepage.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <div id="imagenprincipal">
+                <img src="music.png" alt=""/>
+            </div>
+            <div id="logo">
+                <img src="logo1.png" alt=""/>
+            </div>
+             <div id="buscar">
+            <form action="" autocomplete="" method="POST">
+                <input type="text"  name="buscar"/> <input type="submit" value="BUSCAR">
+            </form>
+            </div>
+            <div id="menucabezera">
+                <ul>
+                    <li><a href="">MENU</a></li>
+                    <li><a href="">NOTICIAS</a></li>
+                    <li><a href="">GRUPOS</a></li>
+                    <li><a href="">CONCIERTOS</a></li>
+                    <li><a href="">ARTICULOS</a></li>
+                    
+                </ul>
+            </div>
+            <div id="entrar">
+                <a href="../login/login.php"><input type="button" value="ENTRAR"></a>
+            </div>
+           
+           
+        </header>
+        
+        <div id="noticias">
+            <h3>Ultimas <b>Noticias</b></h3>
+            <article>
+                <a href=""><img src="../imagenes de noticias/descarga.jpg" alt=""/></a>
+                
+                <a href=""><h4>TITULO</h4></a>
+            </article>
+            <article>
+                <a href=""><img src="../imagenes de noticias/concierto-hombresg-launion-nachapop-zaragoza.jpg" alt=""/></a>                 
+                <a href=""><h4>TITULO</h4></a>
+            </article>
+            <article>
+                <a href=""><img src="../imagenes de noticias/fotos-de-musica.jpg" alt=""/></a>                
+                <a href=""><h4>TITULO</h4></a>
+                
+            </article>
+            <article>
+                <a href=""><img src="../imagenes de noticias/logo1.png" alt=""/></a>
+                
+                <a href=""><h4>TITULO</h4></a>
+            </article>
+        </div>
+        
+        <div id="lateral">
+            <div id="destacados">
+                <h3>Destacados</h3>
+                <div class="listadestacados">
+                    <ol>
+                        <li><a href="">Musico 1</a></li>
+                        <li><a href="">Musico 2</a></li>
+                        <li><a href="">Musico 3</a></li>
+                        <li><a href="">Musico 4</a></li>
+                        <li><a href="">Musico 5</a></li>
+                    </ol>
+                </div>
+            </div>
+            <div id="encuestas">
+                <h3>Encuestas</h3>
+                <div class="listaencuestas">
+                    <ol>
+                        <li><a href="">Encuesta 1</a></li>
+                        <li><a href="">Encuesta 2</a></li>
+                        <li><a href="">Encuesta 3</a></li>
+                        <li><a href="">Encuesta 4</a></li>
+                        <li><a href="">Encuesta 5</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <div id="conciertos">
+            <h3>Lista de Conciertos</h3>
+            <div id="listaconciertos">
+                    <ol>
+                        <li><a href="">Concierto 1</a></li>
+                        <li><a href="">Concierto 2</a></li>
+                        <li><a href="">Concierto 3</a></li>
+                        <li><a href="">Concierto 4</a></li>
+                        <li><a href="">Concierto 5</a></li>
+                    </ol>
+            </div>
+            <div id="imagenconcierto1">
+                <a href=""><img src="../imagenes de noticias/-aprender-musica-adolescencia-noticias.jpg" alt=""/></a>
+                
+            </div>
+        </div>
+        <div id="footer">
+           <div id="legal">
+               <ul>
+                        <li><a href="">Terminos legales 1</a></li>
+                        <li><a href="">Terminos legales 2</a></li>
+                        <li><a href="">Terminos legales 3</a></li>
+                        <li><a href="">Terminos legales 4</a></li>
+                        <li><a href="">Terminos legales 5</a></li>
+               </ul>
+                
+           </div>
+           <div id="contactos">
+                <ul>
+                        <li><a href="">Contacto 1</a></li>
+                        <li><a href="">Contacto 2</a></li>
+                        <li><a href="">Contacto 3</a></li>
+                        <li><a href="">Contacto 4</a></li>
+                        <li><a href="">Contacto 5</a></li>
+               </ul>
+                
+           </div>
+           <div id="nosotros">
+                <ul>
+                        <li><a href="">Datos 1</a></li>
+                        <li><a href="">Datos 2</a></li>
+                        <li><a href="">Datos 3</a></li>
+                        <li><a href="">Datos 4</a></li>
+                        <li><a href="">Datos 5</a></li>
+               </ul>
+                
+           </div>
+        </div>
+  </body>
+
+</html>
