bc32e263f56a6a9b6367f2b85dddb78a96bb09b8
jemb2310 <juanelbertomartin@gmail.com>
2016-11-16 17:37:13 +0100

    he subido conciertos jajajaja!!!!

---
+++ conciertos/conciertos.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link href="../homepage/csshomepage.css" rel="stylesheet" type="text/css"/>
+        <link href="cssconciertos.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <div id="imagenprincipal">
+                <img src="../homepage/music.jpg" alt=""/>
+            </div>
+            <div id="logo">
+                <img src="../homepage/logo1.png" alt=""/>
+            </div>
+             <div id="buscar">
+            <form action="" autocomplete="" method="POST">
+                <input type="text"  name="buscar"/> <input type="submit" value="BUSCAR">
+            </form>
+            </div>
+            <div id="menucabezera">
+                <ul>
+                    <li><a href="">MENU</a></li>
+                    <li><a href="../homepage/homepage.php">NOTICIAS</a></li>
+                    <li><a href="../grupos/grupos.php">GRUPOS</a></li>
+                    <li><a href="../conciertos/conciertos.php">CONCIERTOS</a></li>
+                    <li><a href="">ARTICULOS</a></li>
+                    
+                </ul>
+            </div>
+            <div id="entrar">
+                <a href="../login/login.php"><input type="button" value="ENTRAR"></a>
+            </div>
+        </header>
+        
+        <div id="body">
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            <article><h3>Concierto</h3></article>
+            
+            
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        <div id="footer">
+           <div id="legal">
+               <ul>
+                        <li><a href="">Terminos legales 1</a></li>
+                        <li><a href="">Terminos legales 2</a></li>
+                        <li><a href="">Terminos legales 3</a></li>
+                        <li><a href="">Terminos legales 4</a></li>
+                        <li><a href="">Terminos legales 5</a></li>
+               </ul>
+                
+           </div>
+           <div id="contactos">
+                <ul>
+                        <li><a href="">Contacto 1</a></li>
+                        <li><a href="">Contacto 2</a></li>
+                        <li><a href="">Contacto 3</a></li>
+                        <li><a href="">Contacto 4</a></li>
+                        <li><a href="">Contacto 5</a></li>
+               </ul>
+                
+           </div>
+           <div id="nosotros">
+                <ul>
+                        <li><a href="">Datos 1</a></li>
+                        <li><a href="">Datos 2</a></li>
+                        <li><a href="">Datos 3</a></li>
+                        <li><a href="">Datos 4</a></li>
+                        <li><a href="">Datos 5</a></li>
+               </ul>
+                
+           </div>
+        </div>
+    </body>
+</html>
