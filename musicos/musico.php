3ac5aa219654d1493b72dafa905912906e4bf90a
msdaniel358 <usu32@novaulas.stucom.com>
2016-11-20 17:52:08 +0100

    musicos

---
+++ musicos/musico.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="csshomepage.css" rel="stylesheet" type="text/css"/>
+        <link href="musico.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+
+    <body>
+
+
+        <header>
+            <div id="imagenprincipal">
+                <img src="music.png" alt=""/>
+            </div>
+            <div id="logo">
+                <img src="logo1.png" alt=""/>
+            </div>
+            <div id="buscar">
+                <form action="" autocomplete="" method="POST">
+                    <input type="text"  name="buscar"/> <input type="submit" value="BUSCAR">
+                </form>
+            </div>
+            <div id="menucabezera">
+                <ul>
+                    <li><a href="">MENU</a></li>
+                    <li><a href="">NOTICIAS</a></li>
+                    <li><a href="">GRUPOS</a></li>
+                    <li><a href="">CONCIERTOS</a></li>
+                    <li><a href="">ARTICULOS</a></li>
+
+                </ul>
+            </div>
+            <div id="entrar">
+                <a href="../login/login.php"><input type="button" value="ENTRAR"></a>
+            </div>
+
+
+        </header>
+        <h1>Lista de Conciertos</h1>
+        <div id="rmusic">
+            <div id="formulario1">
+                <div class="datos">
+                    <p> Nombre del local
+                        <input name="nombrelocal" type="text" type="submit">
+                    </p>
+                    <br>
+
+                    <p>  Fecha y Hora
+                        <input name="fecha y hora" type="number" type="submit">
+
+                    </p>
+                    <br>
+
+                    <p> Localizacion: 
+                        <input name="direcicion" type="text" type="submit">
+                    </p>
+                    <br>
+                    <p>Genero: 
+                        <input name="gmusica" type="text" type="submit">
+                    </p>
+
+                </div>
+
+                <div class="estado">
+                    <img src="imagenes/1420218355.jpg" alt=""/>
+                    <br>
+                    estado
+                </div>
+            </div>
+
+            <br>
+
+            <div id="formulario2">
+                <div class="datos2">
+                    <p> Nombre del local
+                        <input name="nombrelocal" type="text" type="submit">
+                    </p>
+                    <br>
+
+                    <p>  Fecha y Hora
+                        <input name="fecha y hora" type="number" type="submit">
+
+                    </p>
+                    <br>
+
+                    <p> Localizacion: 
+                        <input name="direcicion" type="text" type="submit">
+                    </p>
+                    <br>
+                    <p>Genero:<input name="gmusica" type="text" type="submit"></p>
+                </div>
+                <div class="estado2">
+                    <img src="imagenes/conciertos-locales.jpg" alt=""/>
+
+                    <br>
+                    Estado
+                </div>
+            </div>
+
+
+
+        </div>
+
+        <div id="grupos">
+            Grupos de la semana            
+
+        </div>
+
+        <div id="footer">
+            <div id="legal">
+                <ul>
+                    <li><a href="">Terminos legales 1</a></li>
+                    <li><a href="">Terminos legales 2</a></li>
+                    <li><a href="">Terminos legales 3</a></li>
+                    <li><a href="">Terminos legales 4</a></li>
+                    <li><a href="">Terminos legales 5</a></li>
+                </ul>
+
+            </div>
+            <div id="contactos">
+                <ul>
+                    <li><a href="">Contacto 1</a></li>
+                    <li><a href="">Contacto 2</a></li>
+                    <li><a href="">Contacto 3</a></li>
+                    <li><a href="">Contacto 4</a></li>
+                    <li><a href="">Contacto 5</a></li>
+                </ul>
+
+            </div>
+            <div id="nosotros">
+                <ul>
+                    <li><a href="">Datos 1</a></li>
+                    <li><a href="">Datos 2</a></li>
+                    <li><a href="">Datos 3</a></li>
+                    <li><a href="">Datos 4</a></li>
+                    <li><a href="">Datos 5</a></li>
+                </ul>
+
+            </div>
+        </div>
+
+    </body>
+</html>			
