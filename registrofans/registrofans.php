c877f63d3a6e6e7a0f8beb26014156fc142fe391
cc1410 <chen986755611@gmail.com>
2016-11-22 19:42:42 +0100

    otro cambio

--- registrofans/registrofans.php
+++ registrofans/registrofans.php
@@ -27,7 +27,6 @@ and open the template in the editor.
             <div id="menucabezera">
                 <ul>
                     <li><a href="">MENU</a></li>
-                    <br>
                     <li><a href="../homepage/homepage.php">NOTICIAS</a></li>
                     <li><a href="">GRUPOS</a></li>
                     <li><a href="">CONCIERTOS</a></li>
