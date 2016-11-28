99bbe69b4d8aceed7504152bcd93648002486053
msdaniel358 <msdaniel358@gmail.com>
2016-11-28 15:44:50 +0100

    music

---
+++ music/Perfil.php
@@ -0,0 +1,106 @@
+<html lang="en">
+    <head>
+        <link href="csshomepage.css" rel="stylesheet" type="text/css"/>
+        <link href="music.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+
+    <body>
+
+
+        <header>
+            <div id="imagenprincipal">
+                <img src="../homepage/music.jpg" alt=""/>
+            </div>
+            <div id="logo">
+                <img src="../homepage/logo1.png" alt=""/>
+            </div>
+            <div id="buscar">
+                <form action="" autocomplete="" method="POST">
+                    <input type="text"  name="buscar"/> <input type="submit" value="BUSCAR">
+                </form>
+            </div>
+            <div id="menucabezera">
+                <ul>
+                    <li><a href="">MENU</a></li>
+                    <li><a href="../homepage/homepage.php">NOTICIAS</a></li>
+                    <li><a href="../grupos/grupos.php">MI PERFIL</a></li>
+                    <li><a href="../conciertos/conciertos.php">CONCIERTOS</a></li>
+                    <li><a href="">ARTICULOS</a></li>
+
+                </ul>
+            </div>
+            <div id="entrar">
+                <a href="../login/login.php"><input type="button" value="ENTRAR"></a>
+            </div>
+            <div id="login">
+                <img src="imagenes/login.png" alt=""/>
+                <input type="button" value="Musico">
+            </div>
+        </header>
+        <h1>Lista de Conciertos</h1>
+        <div id="rmusic">
+            <div id="formulario1">
+                <div class="datos">
+                    <h1>Perfil </h1>
+                    <div id="imgperf">
+                        <img src="imagenes/PERFIL.png" alt=""/>
+                    </div>
+
+                    <h2>Datos perosonales</h2>
+                    Nombre:
+                    <br>
+                    Apellido:
+                    <br>
+                    Grupo:
+                    <br>
+                    Ciudad:
+                    <br>
+                    Pais:
+                    <br>
+                    Tel:
+                    <br>
+                    Fecha de nacimiento
+                    <br>
+                    Sexo:
+
+                </div>
+
+
+            </div>
+            <div id="footer">
+                <div id="legal">
+                    <ul>
+                        <li><a href="">Terminos legales 1</a></li>
+                        <li><a href="">Terminos legales 2</a></li>
+                        <li><a href="">Terminos legales 3</a></li>
+                        <li><a href="">Terminos legales 4</a></li>
+                        <li><a href="">Terminos legales 5</a></li>
+                    </ul>
+
+                </div>
+                <div id="contactos">
+                    <ul>
+                        <li><a href="">Contacto 1</a></li>
+                        <li><a href="">Contacto 2</a></li>
+                        <li><a href="">Contacto 3</a></li>
+                        <li><a href="">Contacto 4</a></li>
+                        <li><a href="">Contacto 5</a></li>
+                    </ul>
+
+                </div>
+                <div id="nosotros">
+                    <ul>
+                        <li><a href="">Datos 1</a></li>
+                        <li><a href="">Datos 2</a></li>
+                        <li><a href="">Datos 3</a></li>
+                        <li><a href="">Datos 4</a></li>
+                        <li><a href="">Datos 5</a></li>
+                    </ul>
+
+                </div>
+            </div>
+
+    </body>
+</html>			
--- music/music.php
+++ music/music.php
@@ -26,17 +26,17 @@
                 <ul>
                     <li><a href="">MENU</a></li>
                     <li><a href="../homepage/homepage.php">NOTICIAS</a></li>
-                    <li><a href="../grupos/grupos.php">GRUPOS</a></li>
+                    <li><a href="../grupos/grupos.php">MI PERFIL</a></li>
                     <li><a href="../conciertos/conciertos.php">CONCIERTOS</a></li>
                     <li><a href="">ARTICULOS</a></li>
-                    
+
                 </ul>
             </div>
             <div id="entrar">
                 <a href="../login/login.php"><input type="button" value="ENTRAR"></a>
             </div>
-             <div id="login">
-                 <img src="imagenes/login.png" alt=""/>
+            <div id="login">
+                <img src="imagenes/login.png" alt=""/>
                 <input type="button" value="Musico">
             </div>
         </header>
@@ -44,105 +44,139 @@
         <div id="rmusic">
             <div id="formulario1">
                 <div class="datos">
-                    <p> Nombre del local
-                        <input name="nombrelocal" type="text" type="submit">
-                    </p>
-                    <br>
+                    <table id="tabla">
+                        <tr>
+                            <td>Nombre del Local-</td>
 
-                    <p>  Fecha y Hora
-                        <input name="fecha y hora" type="number" type="submit">
+                            <td>Fecha y hora- </td>
 
-                    </p>
-                    <br>
+                            <td>Direccion-</td>
 
-                    <p> Localizacion: 
-                        <input name="direcicion" type="text" type="submit">
-                    </p>
-                    <br>
-                    <p>Genero: 
-                        <input name="gmusica" type="text" type="submit">
-                    </p>
+                            <td>Genero- </td>
+
+                            <td>Estado del concierto </td>
+                        </tr>
+
+                        <tr>
+                            <td> dfdfd</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                        </tr>
+
+                        <tr>
+                            <td> kk</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                        </tr>
+
+                        <tr>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                        </tr>
+
+                        <tr>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                        </tr>
+
+                        <tr>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+
+                        </tr>
+
+                        <tr>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+
+                        </tr>
+
+                        <tr>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                        </tr>
+
+                        <tr>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                        </tr>
+
+                        <tr>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+
+                        </tr>
+
+                        <tr>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                            <td> mm</td>
+                        </tr>
+
+                    </table>
 
                 </div>
 
-                <div class="estado">
-                    <img src="imagenes/1420218355.jpg" alt=""/>
-                    
-                    estado
+
+
+                <div id="footer">
+                    <div id="legal">
+                        <ul>
+                            <li><a href="">Terminos legales 1</a></li>
+                            <li><a href="">Terminos legales 2</a></li>
+                            <li><a href="">Terminos legales 3</a></li>
+                            <li><a href="">Terminos legales 4</a></li>
+                            <li><a href="">Terminos legales 5</a></li>
+                        </ul>
+
+                    </div>
+                    <div id="contactos">
+                        <ul>
+                            <li><a href="">Contacto 1</a></li>
+                            <li><a href="">Contacto 2</a></li>
+                            <li><a href="">Contacto 3</a></li>
+                            <li><a href="">Contacto 4</a></li>
+                            <li><a href="">Contacto 5</a></li>
+                        </ul>
+
+                    </div>
+                    <div id="nosotros">
+                        <ul>
+                            <li><a href="">Datos 1</a></li>
+                            <li><a href="">Datos 2</a></li>
+                            <li><a href="">Datos 3</a></li>
+                            <li><a href="">Datos 4</a></li>
+                            <li><a href="">Datos 5</a></li>
+                        </ul>
+
+                    </div>
                 </div>
-            </div>
 
-            <br>
-
-            <div id="formulario2">
-                <div class="datos2">
-                    <p> Nombre del local
-                        <input name="nombrelocal" type="text" type="submit">
-                    </p>
-                    <br>
-
-                    <p>  Fecha y Hora
-                        <input name="fecha y hora" type="number" type="submit">
-
-                    </p>
-                    <br>
-
-                    <p> Localizacion: 
-                        <input name="direcicion" type="text" type="submit">
-                    </p>
-                    <br>
-                    <p>Genero:<input name="gmusica" type="text" type="submit"></p>
-                </div>
-                <div class="estado2">
-                    <img src="imagenes/conciertos-locales.jpg" alt=""/>
-
-                   
-                    Estado
-                </div>
-            </div>
-
-
-
-        </div>
-
-        <div id="grupos">
-            <h1> Grupos de la semana </h1>
-                        
-
-        </div>
-
-        <div id="footer">
-            <div id="legal">
-                <ul>
-                    <li><a href="">Terminos legales 1</a></li>
-                    <li><a href="">Terminos legales 2</a></li>
-                    <li><a href="">Terminos legales 3</a></li>
-                    <li><a href="">Terminos legales 4</a></li>
-                    <li><a href="">Terminos legales 5</a></li>
-                </ul>
-
-            </div>
-            <div id="contactos">
-                <ul>
-                    <li><a href="">Contacto 1</a></li>
-                    <li><a href="">Contacto 2</a></li>
-                    <li><a href="">Contacto 3</a></li>
-                    <li><a href="">Contacto 4</a></li>
-                    <li><a href="">Contacto 5</a></li>
-                </ul>
-
-            </div>
-            <div id="nosotros">
-                <ul>
-                    <li><a href="">Datos 1</a></li>
-                    <li><a href="">Datos 2</a></li>
-                    <li><a href="">Datos 3</a></li>
-                    <li><a href="">Datos 4</a></li>
-                    <li><a href="">Datos 5</a></li>
-                </ul>
-
-            </div>
-        </div>
-
-    </body>
-</html>			
+                </body>
+                </html>			
